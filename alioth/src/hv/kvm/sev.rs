e75270164f63c98974c67cb824b6e87c12c7ad55
Changyuan Lyu <changyuanl@google.com>
2025-10-13 16:18:19 -0700

    refactor(sev): move AMD-SEV bindings to mod sys and arch
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/hv/kvm/sev.rs
+++ alioth/src/hv/kvm/sev.rs
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(dead_code)]
-
-pub mod bindings;
-
 use std::fmt::Debug;
 use std::fs::File;
 use std::os::fd::{AsFd, BorrowedFd, OwnedFd};
@@ -25,13 +21,7 @@ use snafu::ResultExt;
 
 use crate::hv::Result;
 use crate::hv::kvm::kvm_error;
-use crate::ioctl_writeread;
-
-use self::bindings::{SEV_RET_SUCCESS, SevIssueCmd};
-
-const SEV_IOC_TYPE: u8 = b'S';
-
-ioctl_writeread!(sev_issue_cmd, SEV_IOC_TYPE, 0x0, SevIssueCmd);
+use crate::sys::sev::{SEV_RET_SUCCESS, SevIssueCmd, sev_issue_cmd};
 
 #[derive(Debug)]
 pub struct SevFd {
@@ -44,8 +34,6 @@ impl AsFd for SevFd {
     }
 }
 
-pub const SEV_PLATFORM_STATUS: u32 = 1;
-
 impl SevFd {
     pub fn new(path: impl AsRef<Path>) -> Result<Self> {
         let f = File::open(&path).context(kvm_error::OpenFile {
@@ -55,6 +43,7 @@ impl SevFd {
         Ok(sev_fd)
     }
 
+    #[allow(dead_code)]
     pub fn issue_cmd<T>(&self, cmd: u32, data: &mut T) -> Result<()> {
         let mut req = SevIssueCmd {
             cmd,
